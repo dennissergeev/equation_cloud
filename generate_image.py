771908227fa5efaf89a9d3179a4e699cd2fcf865
Dennis <dennis.sergeev@gmail.com>
2016-08-01 22:50:28 +0100

    loop over all available equations

--- generate_image.py
+++ generate_image.py
@@ -2,6 +2,7 @@
 from preamble import load
 load()
 import matplotlib.pyplot as plt  # NOQA
+import matplotlib.patheffects as PathEffects  # NOQA
 import numpy as np  # NOQA
 
 
@@ -11,20 +12,26 @@ with open("equations.tex") as f:
         eq = r"${}$".format(line)
         eqs.append(eq)
 
-facecolor = '#000000'
-fontcolor = '#1234AB'
+facecolor = '#fff9ff'
+fontcolor = '#333333'
+# strokecolor = '#FFFFFF'
+
+txt_dict = dict(ha='center', va='center', color=fontcolor,
+                clip_on=True)
+# pe_dict = dict(linewidth=1, foreground=strokecolor)
 
 fig, ax = plt.subplots(figsize=(10, 10))
-txt_dict = dict(ha='center', va='center', color=fontcolor,
-                transform=ax.transAxes, clip_on=True)
 
-for i in range(5):
-    idx = np.random.randint(0, len(eqs))
-    eq = eqs[idx]
-    size = np.random.uniform(12, 32)
+for eq in eqs:
+    # idx = np.random.randint(0, len(eqs))
+    # eq = eqs[idx]
+    size = np.random.uniform(10, 30)
     x, y = np.random.uniform(0, 1, 2)
-    alpha = np.random.uniform(0.5, .95)
-    ax.text(x, y, eq, alpha=alpha, **txt_dict)
+    alpha = np.random.uniform(0, 1)
+    t = ax.text(x, y, eq, alpha=alpha, size=size,
+                transform=ax.transAxes, **txt_dict)
+    # t.set_path_effects([PathEffects.withStroke(**pe_dict)])
+
 
 ax.axis('off')
 fig.savefig('figure.pdf', facecolor=facecolor)
